b12aaa51cb78e60e8451cb70f16c81a16d86a067
EmilePW <emilepw@hotmail.co.uk>
2016-04-16 17:20:44 +0100

    Replaced single quotes with double quotes in index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var router = express.Router();
-var sass = require('node-sass');
+var sass = require("node-sass");
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + "/app"));
 
 // Serves up index page for SPA
-app.get('*', function(req, res) {
-	res.sendFile('index.html', {root: __dirname});
+app.get("*", function(req, res) {
+	res.sendFile("index.html", {root: __dirname});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('Woop! Woop! Join the party on localhost: ' + port);
+console.log("Woop! Woop! Join the party on localhost: " + port);
 
 exports = module.exports = app;
